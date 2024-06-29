beccbc722bd48ae80920b4cf3c54fcb32a2abbc5
madusfx <mariaesfreitas@gmail.com>
2024-06-29 17:02:16 -0300

    adding about me

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import resume from './assets/files/resume.pdf';
 
 function App() {
   return (
@@ -51,6 +52,31 @@ function App() {
             </button>
           </div>
         </div>
+      </div>
+      <div className="flex flex-col mt-24 items-baseline">
+        <p className="text-indigo-400 text-3xl font-['Preahvihear']">
+          Sobre mim
+        </p>
+        <p className="text-xl mt-4">Ponta Grossa, Brasil</p>
+        <p className="text-left mt-4">
+          Meu nome é Maria Eduarda, tenho 22 anos e sou paranaense. Minha
+          história com a programação começou em 2020, quando eu entrei na
+          faculdade, sem saber nada da área de tecnologia. Ao longo do curso fui
+          me apaixonando e me encontrando profissionalmente e hoje sou muito
+          realizada com o que eu faço. Com o fim da faculdade, tenho buscado
+          fazer cursos para aprimorar meus conhecimentos e tenho trabalhado em
+          alguns projetos pessoais também, além de ter dado início a uma pós de
+          Engenharia de Software Ágil pela PUCPR.
+          <br />
+          Nas horas vagas eu costumo gostar de praticar esportes, principalmente
+          musculação e corrida. Além disso, sou uma grande fã de sitcoms, e
+          tenho uma paixão especial por Friends.
+        </p>
+        <button className="bg-indigo-400 py-2 px-6 rounded mt-12">
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            Baixar currículo
+          </a>
+        </button>
       </div>
     </div>
   );
