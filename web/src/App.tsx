9bbfd8f6a91cd4b72a4f7961aca604889857a015
madusfx <mariaesfreitas@gmail.com>
2024-06-29 12:09:27 -0300

    creating header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,26 @@ import './App.css';
 function App() {
   return (
     <>
-      <p className="text-red-600">Hello World</p>
+      <div className="w-9/12 flex justify-between">
+        <p className="text-indigo-400 text-2xl font-['Preahvihear']">
+          Maria Eduarda Freitas
+        </p>
+        <div className="flex space-x-6">
+          <a href="" className="text-lg">
+            Início
+          </a>
+          <a href="" className="text-lg">
+            Sobre mim
+          </a>
+          <a href="" className="text-lg">
+            Experiências
+          </a>
+          <a href="" className="text-lg">
+            Projetos
+          </a>
+        </div>
+      </div>
+      <div className="w-9/12 h-0.5 bg-slate-800 mt-6" />
     </>
   );
 }
