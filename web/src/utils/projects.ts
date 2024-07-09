c989b7dd2a0d4013cf6bead53154bf14d9844b12
madusfx <mariaesfreitas@gmail.com>
2024-07-09 17:12:28 -0300

    updating projects

--- web/src/utils/projects.ts
+++ web/src/utils/projects.ts
@@ -5,6 +5,8 @@ export const projects = [
       'Aplicativo para gestão de hábitos, possui login com cadastro de usuário, funcionalidades de criação, edição, exclusão e conclusão de um hábito, visualização do histórico de hábitos completos e funções de edição e exclusão de usuário. É um projeto Full Stack que foi criado como parte do meu trabalho de conclusão de curso.',
     technologies:
       'REACT NATIVE • EXPO GO • STYLED COMPONENTS • NODE • NEST • POSTGRESQL • DOCKER',
+    projectUrl:
+      'https://drive.google.com/file/d/1ZmQSuW2CgwIPwc-msk-OVZENRP1vqGEO/view?usp=sharing',
     repository: 'https://github.com/madusfx/habitsync',
   },
   {
@@ -12,6 +14,16 @@ export const projects = [
     description:
       'Landing page feita para um escritório de advocacia, contém as principais informações do escritório e os meios de contato.',
     technologies: 'REACT • STYLED COMPONENTS • NODE ',
+    projectUrl: 'https://moraes-adv.vercel.app/',
     repository: 'https://github.com/madusfx/moraes-adv',
+  },
+  {
+    title: 'TMDB Movies',
+    description:
+      'Aplicativo que mostra seleção dos filmes populares do momento, contém funcionalidade de busca e possui dark e light mode. É conectado com a API aberta The Movie Database (TMDB)',
+    technologies: 'REACT • STYLED COMPONENTS ',
+    projectUrl:
+      'https://drive.google.com/file/d/1CC4RJeppDe60EA5k-NxSqln_D6A_s1-J/view?usp=sharing',
+    repository: 'https://github.com/madusfx/react-native-developer-challenge',
   },
 ];
